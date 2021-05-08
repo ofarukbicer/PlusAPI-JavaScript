f2fc19112cb3d83256db6fedceb8647c305dd9ff
Ömer Faruk Biçer <omerfarukbicer0446@gmail.com>
2021-05-08 08:09:02 +0300

    [v1.0.0]

---
+++ README.md
@@ -0,0 +1,21 @@
+# 🗄️ PlusAPI-nodejs
+
+## 📥 Kurulum
+
+`git clone https://www.github.com/ofarukbicer/PlusAPI-nodejs`
+
+## 📒 Kullanım
+
+Çok Yakında
+
+## 🌐 Telif Hakkı ve Lisans
+
+* *Copyright (C) 2021 by* [@ofarukbicer](https://github.com/ofarukbicer) ❤️️
+* [MIT LICENSE](https://github.com/ofarukbicer/plusapi/blob/master/LICENSE) *Koşullarına göre lisanslanmıştır..*
+
+## ♻️ İletişim
+
+*Bizimle iletişime geçmek isterseniz, **Telegram**'dan mesaj göndermekten çekinmeyin;* [@PlusAPI](https://t.me/PlusAPI)
+
+
+> **[PlusAPI](https://plusapi.org)** *için yazılmıştır..*
---
+++ index.js
@@ -0,0 +1,123 @@
+'use strict';
+
+const axios = require('axios');
+
+class PlusAPI {
+
+  /**
+   * 
+   * PlusAPI | Piyasa Verileri Sınıfı
+   *   
+   * Gereksinimler
+   * -------------
+   *  $token = XXXX
+   *  
+   * Methodlar
+   * ---------
+   *  hisse_ver($sembol) -> İstenilen Sembolün Bilgilerini Çevirir
+   * 
+   *  hisse_sepet() -> BIST Top-10 Çevirir
+   * 
+   *  kripto_ver($sembol) -> İstenilen Sembolün Bilgilerini Çevirir
+   * 
+   *  kripto_haber() -> Kriptolar Hakkında Güncel Haberleri Çevirir
+   * 
+   *  kripto_sepet() -> Kripto Top-10 Çevirir
+   * 
+   *  bilgi -> "PlusAPI | Piyasa Verileri Sınıfı"
+   * 
+   *  token -> Girdiğiniz tokeni geriye dönderir
+   * 
+   * @param {*} token Bizim tarafızdan size verilen token
+   */
+  constructor(token) {
+    this.token = token;
+    this.bilgi = "PlusAPI | Piyasa Verileri Sınıfı";
+  }
+
+  /**
+   * 
+   * PlusAPI'nin API hizmetine istek atar
+   * 
+   * @param {*} endpoint İstek atmak istediğiniz endpoint
+   * @param {*} sembol Eğer istek atmak istediğiniz endpoint sembol isterse doldurulması gerekir
+   * @returns 
+   */
+  async istek_at(endpoint, sembol = null) {
+    await axios({
+      method: 'get',
+      url: "https://plusapi.org/api" + endpoint + "?token=" + this.token + (sembol != null ? "&sembol=" + encodeURIComponent(sembol) : ""),
+      responseType: 'json'
+    }).then(async (response) => {
+      global.data = await response.data;
+    })
+    return global.data;
+  }
+
+  /**
+   * 
+   * İstenilen Sembolün Bilgilerini Çevirir
+   * 
+   * @param {*} sembol 
+   * @returns 
+   */
+  async hisse_ver(sembol = "") {
+    await this.istek_at("/hisse", sembol).then(async (data) => {
+      global.data = await data;
+    })
+    return global.data;
+  }
+
+  /**
+   * 
+   * BIST Top-10 Çevirir
+   * 
+   * @returns 
+   */
+  async hisse_sepet() {
+    await this.istek_at("/hisse/sepet").then(async (data) => {
+      global.data = await data;
+    })
+    return global.data;
+  }
+
+  /**
+   * 
+   * İstenilen Sembolün Bilgilerini Çevirir
+   * 
+   * @param {*} sembol 
+   * @returns 
+   */
+  async kripto_ver(sembol = "") {
+    await this.istek_at("/kripto", sembol).then(async (data) => {
+      global.data = await data;
+    })
+    return global.data;
+  }
+
+  /**
+   * 
+   * Kriptolar Hakkında Güncel Haberleri Çevirir
+   * 
+   * @returns
+   */
+  async kripto_haber() {
+    await this.istek_at("/kripto/haber").then(async (data) => {
+      global.data = await data;
+    })
+    return global.data;
+  }
+
+  /**
+   * 
+   * Kripto Top-10 Çevirir
+   * 
+   * @returns
+   */
+  async kripto_sepet() {
+    await this.istek_at("/kripto/sepet").then(async (data) => {
+      global.data = await data;
+    })
+    return global.data;
+  }
+}
